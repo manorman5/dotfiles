eb13b80e5cd285e8323e852f279b50be63cf9e07
mayaanorman <mayaanorman@local>
2019-11-25 21:42:24 +0000

    personalizing dotfiles repo for my own use

--- init.sh
+++ init.sh
@@ -2,22 +2,8 @@
 
 DIR=$(cd `dirname $0` && pwd)
 
-# warn about zsh and ohmyzsh if not installed 
-if ! [[ -f "~/.zsh_history" ]]; then
-   echo "WARNING: Zsh not installed."
-fi
-
-if ! [[ -f "~/.oh-my-zsh" ]]; then
-    echo "WARNING: ohmyzsh not installed."
-else 
-     # if zsh and ohmyzsh installed, run setup. 
-    ln -sf $DIR/zsh/zshrc ~/.zshrc
-    source ~/.zshrc
-fi
-
 # create symlinks
-# ln -sf $DIR/bash/profile ~/.bash_profile
-ln -sf $DIR/git/gitconfig ~/.gitconfig
+ln -sf $DIR/bash/bashrc ~/.bashrc
 ln -sf $DIR/vim/vimrc ~/.vimrc
 
 
