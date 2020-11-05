96631755eb0776cc38099b1eec3dd3edd4f7ae82
GIT SERVICE NAME (NOT USERNAME) <42501578+manorman5@users.noreply.github.com>
2020-11-05 15:31:55 -0800

    updating for zsh

--- init.sh
+++ init.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
-#testing
-DIR=$(cd `dirname $0` && pwd)
+DOTFILE_DIR=$(cd `dirname $0` && pwd)
+
+# download oh my zsh
+if [ ! -d ~/.oh-my-zsh ]; then
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
 
 # create symlinks
 
-for f in $DIR/bash/*
-do
-    ls -sf $f ~/.$(basename $f)
-done
-ln -sf $DIR/vim/vimrc ~/.vimrc
-
+ln -sf $DOTFILE_DIR/zsh/zshrc ~/.zshrc
+ln -sf $DOTFILE_DIR/vim/vimrc ~/.vimrc
+ln -sf $DOTFILE_DIR/git/gitignore ~/.gitignore
 
